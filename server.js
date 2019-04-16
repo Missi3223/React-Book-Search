978ac2d4c020e3d333f24f7336d7ca279706fe4c
Melissa Stringer <44407731+Missi3223@users.noreply.github.com>
2019-04-16 00:16:48 -0600

    Missi (#1)
    * fresh
    
    * merging

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const databaseUri = "mongodb://localhost/BookSearch";
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
