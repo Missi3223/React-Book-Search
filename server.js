54b960c214d436eb96ed6fea8abced3e6c333004
Melissa Stringer <honeybee3223@gmail.com>
2019-04-16 00:14:27 -0600

    merging

--- server.js
+++ server.js
@@ -2,18 +2,13 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-<<<<<<< HEAD
 
-=======
 const databaseUri = "mongodb://localhost/BookSearch";
->>>>>>> 7f727045987931c5f7f2ff4e7695601ddbc33c52
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-<<<<<<< HEAD
-=======
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
@@ -21,7 +16,6 @@ if (process.env.MONGODB_URI) {
 }
 console.log(databaseUri);
 
->>>>>>> 7f727045987931c5f7f2ff4e7695601ddbc33c52
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
