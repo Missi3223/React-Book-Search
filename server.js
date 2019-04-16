607f7ca309d317d7c8095766bfe3ee565ad77c47
Melissa Stringer <honeybee3223@gmail.com>
2019-04-15 23:40:18 -0600

    Heroku_Deployed

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const databaseUri = "mongodb://localhost/BookSearch";
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
+console.log(databaseUri);
 
 // Send every request to the React app
 // Define any API routes before this runs
